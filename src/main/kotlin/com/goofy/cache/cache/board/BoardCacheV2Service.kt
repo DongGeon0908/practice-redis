d257136e512114fcdb5a77b8e632ed7a4fce378f
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2022-07-12 21:24:13 +0900

    expired time 관련 cache 처리 추가

--- src/main/kotlin/com/goofy/cache/cache/board/BoardCacheV2Service.kt
+++ src/main/kotlin/com/goofy/cache/cache/board/BoardCacheV2Service.kt
@@ -2,6 +2,8 @@ package com.goofy.cache.cache.board
 
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.ZoneId
 
 /**
  * Redis 예제용 Cache Service
@@ -26,4 +28,29 @@ class BoardCacheV2Service(
      * value를 1씩 감소시킴
      **/
     fun decrease(type: Long) = redis.opsForValue().decrement(BoardCacheV2Key.CACHE_EXAMPLE.key(type))
+
+
+    /**
+     * key-value를 생성하면서 expired time을 key에 등록
+     **/
+    fun setAndExpired(type: Long) {
+        redis.opsForValue().set(
+            BoardCacheV2Key.CACHE_EXAMPLE.key(type),
+            "example value"
+        )
+
+        val expired = LocalDate.now()
+            .atTime(23, 59, 59)
+            .atZone(ZoneId.of("Asia/Seoul"))
+            .toInstant()
+
+        redis.expireAt(BoardCacheV2Key.CACHE_EXAMPLE.key(type), expired)
+    }
+
+    /**
+     * key에 대한 expired time 확인
+     **/
+    fun getExpired(type: Long): Long {
+        return redis.getExpire(BoardCacheV2Key.CACHE_EXAMPLE.key(type))
+    }
 }

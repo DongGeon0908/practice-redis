040eeec1fa5b873f6806f603fcb8ca6a726d1936
극락코딩 <50691225+DongGeon0908@users.noreply.github.com>
2023-02-22 14:44:56 +0900

    Update RedisPubSub.kt

--- src/main/kotlin/com/goofy/cache/event/RedisPubSub.kt
+++ src/main/kotlin/com/goofy/cache/event/RedisPubSub.kt
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component
 class RedisPublisher(
     private val redisTemplate: RedisTemplate<String, Any>
 ) {
+    /** 특정 채널에 Message 발행 */
     fun publish(channel: ChannelTopic, message: Any) {
         redisTemplate.convertAndSend(channel.topic, message)
     }
@@ -21,6 +22,7 @@ class RedisPublisher(
 class RedisSubscriber(
     private val redisTemplate: RedisTemplate<String, Any>
 ) : MessageListener {
+    /** 특정 채널의 메세지 구독 */
     override fun onMessage(message: Message, pattern: ByteArray?) {
         val channel = redisTemplate.getChannel(message)
         val content = redisTemplate.getMessage(message)
@@ -31,16 +33,19 @@ class RedisSubscriber(
     }
 }
 
+/** 발행 이벤트 메세지 */
 data class EventModel(
     val id: Long,
     val description: String
 )
 
+/** 채널 정보 조회 */
 fun RedisTemplate<String, Any>.getChannel(message: Message): String {
     return this.stringSerializer.deserialize(message.channel)
         ?: throw RedisPubSubException("channel is null or empty")
 }
 
+/** 메세지 조회 */
 fun RedisTemplate<String, Any>.getMessage(message: Message): EventModel {
     val messageBody = this.stringSerializer.deserialize(message.body)
         ?: throw RedisPubSubException("message is null or empty")
